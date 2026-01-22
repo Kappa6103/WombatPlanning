2e80e48bf5be9cbdbaa2f6fadd1cb95d801d5ba5
Kevin <schadekevin@protonmail.com>
2026-01-22 20:51:12 +0100

    starting to work on Entity's repositories and services

---
+++ src/main/java/com/wombatplanning/services/UserService.java
@@ -0,0 +1,17 @@
+package com.wombatplanning.services;
+
+import com.wombatplanning.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+class UserService {
+
+    private final UserRepository userRepository;
+
+    private void test() {
+        userRepository.findAll().getFirst().getChantierSet();
+    }
+
+}
