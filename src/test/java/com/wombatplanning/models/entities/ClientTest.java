c6f761a04bba740b58f6957f0ee252457668bcd9
Kevin desktop <schadekevin@protonmail.com>
2026-01-25 21:08:23 +0100

    swapping templating engine, from jte to Thymeleaf, adding few services.

--- src/test/java/com/wombatplanning/models/entities/ClientTest.java
+++ src/test/java/com/wombatplanning/models/entities/ClientTest.java
@@ -6,10 +6,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ClientTest {
 
+    private final String clientName = "client name";
+
     @Test
     void createClient() {
         User user = UserFactory.createUser();
+        Client client1 = Client.create(user, clientName);
+        Client client2 = Client.create(user, clientName);
 
+        assertTrue(user.getClientSet().contains(client1));
+        assertEquals("name", client1.getUserName());
+        assertEquals(clientName, client1.getName());
 
     }
 
