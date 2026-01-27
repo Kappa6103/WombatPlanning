d8bb1e1a322c48311d11fefa6a3ed8143b837820
Kevin desktop <schadekevin@protonmail.com>
2026-01-27 23:29:38 +0100

    working on first page draft.

--- src/main/java/com/wombatplanning/web/controllers/controllers.java
+++ src/main/java/com/wombatplanning/web/controllers/controllers.java
@@ -1,18 +1,25 @@
 package com.wombatplanning.web.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@NullMarked
 @Controller
 @RequiredArgsConstructor
 public class controllers {
 
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(@AuthenticationPrincipal UserDetails userDetails, Model model) {
+        String username = userDetails.getUsername();
+        model.addAttribute("username", username);
         return "index";
     }
+
 
 }
